bf15618fe170c5e8c13385565caf5765642b38b7
Luke Kaalim <lukekaalim@gmail.com>
2019-06-05 18:26:10 +1000

    use commonjs rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from "rollup-plugin-terser";
 import flowEntry from 'rollup-plugin-flow-entry'
+import commonjs from 'rollup-plugin-commonjs';
 
 const rollupConfig = {
   input: 'src/index.js',
@@ -9,7 +10,7 @@ const rollupConfig = {
     { format: 'umd', file: 'dist/atlas-client.umd.js', name: 'ATLAS_CLIENT' },
     { format: 'esm', file: 'dist/atlas-client.esm.js' },
   ],
-  plugins: [resolve(), terser(), flowEntry()],
+  plugins: [resolve(), commonjs(), terser(), flowEntry()],
 };
 
 export default rollupConfig;
