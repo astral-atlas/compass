c061966e1b9e65e26fcb1d107472796c165f4950
Luke Kaalim <lukekaalim@gmail.com>
2019-06-07 17:34:56 +1000

    Remove flow intergration

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
 import resolve from 'rollup-plugin-node-resolve';
-import { terser } from "rollup-plugin-terser";
-import flowEntry from 'rollup-plugin-flow-entry'
-import commonjs from 'rollup-plugin-commonjs';
 
 const rollupConfig = {
   input: 'src/index.js',
@@ -10,7 +7,7 @@ const rollupConfig = {
     { format: 'umd', file: 'dist/atlas-client.umd.js', name: 'ATLAS_CLIENT' },
     { format: 'esm', file: 'dist/atlas-client.esm.js' },
   ],
-  plugins: [resolve(), commonjs(), terser(), flowEntry()],
+  plugins: [resolve()],
 };
 
 export default rollupConfig;
