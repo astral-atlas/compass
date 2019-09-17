483e49d1d09179761c4e78ba35e546006805e45e
Luke Kaalim <lukekaalim@gmail.com>
2019-09-17 16:34:23 +1000

    Remove old unused functionality

--- src/compass.js
+++ src/compass.js
@@ -1,43 +1,23 @@
 // @flow
-import { toArray } from './lib/types';
-import { toUser } from './user';
 import { createSessionClient } from './sessionClient';
 
 /*::
-import type { User } from './user';
-import type { Requester } from './lib/http';
-import type { Base64Encoder } from './lib/encoder';
-import type { Authentication } from './lib/authentication';
-
+import type { HTTPClient } from '@lukekaalim/http-client';
 import type { SessionClient } from './sessionClient';
 
 type Compass = {
-  getUsers: () => Promise<Array<User>>,
   session: SessionClient,
 };
 */
 
-const toGetUsersResponse = toArray(toUser);
-
 export const createCompass = (
   domain/*: string*/,
-  requester/*: Requester*/,
-  base64Encoder/*: Base64Encoder*/,
-  authentication/*: Authentication*/
+  client/*: HTTPClient*/,
 )/*: Compass*/ => {
-  const getUsers = async () => {
-    const getUsersRequest = await requester.startRequest(domain, '/users', [], []);
-    if (getUsersRequest.type === 'failure') {
-      throw getUsersRequest.failure;
-    }
-    const requestContents = toGetUsersResponse(JSON.parse(getUsersRequest.success));
-    return requestContents;
-  };
 
-  const session = createSessionClient(domain, requester);
+  const session = createSessionClient(domain, client);
 
   return {
-    getUsers,
     session,
   };
 };
--- src/lib/encoder.js
+++
@@ -1,6 +0,0 @@
-// @flow
-/*::
-import type { Result } from './result';
-
-export type Base64Encoder = (stringToEncode: string) => Result<string, Error>;
-*/
