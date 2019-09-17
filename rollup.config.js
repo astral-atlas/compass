21a10184d8df9aa00f45171d9388f001623d7502
Luke Kaalim <lukekaalim@gmail.com>
2019-09-17 16:34:50 +1000

    Update deps and outputs

--- rollup.config.js
+++ rollup.config.js
@@ -6,9 +6,9 @@ const { terser } = require('rollup-plugin-terser');
 const rollupConfig = {
   input: 'src/index.js',
   output: [
-    { format: 'cjs', file: 'dist/atlas-client.cjs.js' },
-    { format: 'umd', file: 'dist/atlas-client.umd.js', name: 'ATLAS_CLIENT' },
-    { format: 'esm', file: 'dist/atlas-client.esm.js' },
+    { format: 'cjs', file: 'dist/compass.cjs.js' },
+    { format: 'umd', file: 'dist/compass.umd.js', name: 'ATLAS_CLIENT' },
+    { format: 'esm', file: 'dist/compass.esm.js' },
   ],
   plugins: [resolve(), flowEntry(), commonjs(), terser()],
 };
